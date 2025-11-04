df8b1e9a96d1306dca549c353b22eab2ca090055
tarirogwesu82-ai <tarirogwesu82@gmail.com>
2025-11-04 20:07:58 +0200

    Correct accordion and tab functions in main.jsi
    Fix syntax errors and improve accordion and tab functionality.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+//Accord Fun 
+document.querySelectorAll("accordion").forEach(acc) => {
+ acc.addEventListener("click", function () {
+ this.classList.toggle("active");
+   let panel = this.nextElementSibling;
+   panel.style.display = panel.style.display === "block" ? "none" : "block"
+   });
+}};
 
+//tabs
+functions opentab(evt, tabname) {
+  const tabcontent = document.querySelectionAll("tab-content");
+  tabcontent.forEach{(tab) => (tab.style.display = none);
+  document.queryselectorAll('.tabs-link').forEach(tab => tab.classList).remove('')
+}
